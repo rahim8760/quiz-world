4dc6561dcd80266934c409e77eb96c063851f2df
md abdur rahim <rahim8760>
2022-10-10 23:02:54 +0600

    components add

---
+++ src/components/Coursesanalyties/Coursesanalyties.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Coursesanalyties.css'
+
+const Coursesanalyties = () => {
+    return (
+        <div>
+            <h1>hallo from Courses Analyties </h1>
+        </div>
+    );
+};
+
+export default Coursesanalyties;
